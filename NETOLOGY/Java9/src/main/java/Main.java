04a3a522ef250e9e4fafe8fdb4f65e9f6d6fc2b7
Malikosss <malikosss@yandex.ru>
2022-10-15 10:26:43 +0300

    first commit

---
+++ NETOLOGY/Java9/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Анна";
+        post.passport = "1234 №112234";
+        post.patronymic = "Анновна";
+        post.phone = "+7 (926)-304-06-62";
+        post.surname = "Тен";
+        post.subscription = true;
+        post.birthday.day = 10;
+        post.birthday.month = 10;
+        post.birthday.year = 1999;
+
+    }
+}
